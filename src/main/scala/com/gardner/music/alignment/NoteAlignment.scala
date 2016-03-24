5fafee43f85073a7e4337f4d879dad87a2fb2257
Matt Gardner <mattg@allenai.org>
2016-03-23 22:40:34 -0700

    Finally just removed the old java code
    I figured that I needed to start from scratch.  If I want to add back in the
    transcription functionality some day, I can just take it from the other repo.

--- src/main/scala/com/gardner/music/alignment/NoteAlignment.scala
+++
@@ -1,6 +0,0 @@
-package com.gardner.music.alignment
-
-import com.gardner.music.core.Note
-import com.gardner.music.transcription.TranscribedNote
-
-class NoteAlignment(pairs: Seq[(Option[Note], Option[TranscribedNote])])
